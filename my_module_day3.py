502d60c53d8536ea9ca678097241d76c7c6715c5
Pearl Osborne <pearlosborne@hotmail.com>
2023-09-10 09:36:10 -0500

    hw done

---
+++ my_module_day3.py
@@ -0,0 +1,13 @@
+def house():
+    length = int(input('Enter the length: '))
+    width = int(input('Enter the width: '))
+    area = length * width
+    print(area)
+    
+
+import math 
+
+def circle():
+    diameter = int(input('Enter the diameter of the circle:  '))
+    circumference = diameter * math.pi 
+    print(circumference)
